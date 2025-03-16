5489362666cc2a168c0fb8afdfb212d46dfc24d5
Karina <[karinakios@mail.ru]>
2025-03-16 12:10:00 +0300

    list of shop status

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,31 @@
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws IOException {
         OkHttpClient client = new OkHttpClient();
-        Request request1 = new Request.Builder().get().url("http://localhost:8082/application/status").build();
-        String responseBody1 = client.newCall(request1).execute().body().string();
-        System.out.println(responseBody1);
+        createListInfo(client);
+    }
 
-        Request request2 = new Request.Builder().get().url("http://localhost:8081/application/status").build();
+    public static String checkSeven(OkHttpClient client) throws IOException {
+        Request request1 = new Request.Builder().get().url("http://localhost:8081/application/status").build();
+        String responseBody1= client.newCall(request1).execute().body().string();
+        return responseBody1;
+    }
+
+    public static String checkNine(OkHttpClient client) throws IOException {
+        Request request2 = new Request.Builder().get().url("http://localhost:8082/application/status").build();
         String responseBody2 = client.newCall(request2).execute().body().string();
-        System.out.println(responseBody2);
+        return responseBody2;
+    }
 
-
-
+    public static List<String> createListInfo(OkHttpClient client) throws IOException {
+        List<String> infoList = new ArrayList<>();
+        infoList.add(checkSeven(client));
+        infoList.add(checkNine(client));
+        return infoList;
     }
 }
